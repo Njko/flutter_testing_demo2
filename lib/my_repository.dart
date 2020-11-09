f944a27c5f3bdf3915ca09e5766a2d075fa71a52
Nicolas Linard <nicolas.linard@axa.fr>
2020-11-09 17:56:42 +0100

    fix : [US] updated the project fixed the examples based on the article

---
+++ lib/my_repository.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class Post {
+  dynamic data;
+  Post.fromJson(this.data);
+}
+
+class FetchPostRequestException implements Exception {
+  String cause;
+  FetchPostRequestException(this.cause);
+}
+
+class MyRepository {
+  final http.Client client;
+
+  MyRepository(this.client);
+
+  Future<Post> fetchPost(
+      {String requestString =
+          'https://jsonplaceholder.typicode.com/posts/1'}) async {
+    /*final response = await client.get(requestString);
+    final decodedResponse = jsonDecode(response.body);
+
+    return Post.fromJson(decodedResponse);*/
+    //3
+    /*final response =
+        await client.get('https://jsonplaceholder.typicode.com/posts/1');
+
+    if (response.statusCode == 200) {
+      // If the call to the server was successful, parse the JSON.
+      return Post.fromJson(jsonDecode(response.body));
+    } else {
+      // If that call was not successful, throw an error.
+      throw Exception('Failed to load post');
+    }*/
+
+    //4*/
+    //try {
+    final response = await client.get(requestString);
+
+    if (response.statusCode == 200) {
+      // If the call to the server was successful, parse the JSON.
+      return Post.fromJson(jsonDecode(response.body));
+    } else {
+      // If that call was not successful, throw an error.
+      throw FetchPostRequestException('Not not found');
+    }
+    //} finally {
+    //  client.close();
+    //}
+  }
+}
--- test/fetch_post_test.dart
+++ test/fetch_post_test.dart
@@ -1,17 +1,20 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:http/http.dart' as http;
-import 'package:flutter_testing_demo2/fetch_post.dart';
+import 'package:flutter_testing_demo2/my_repository.dart';
+import 'package:matcher/matcher.dart' as matcher;
 
 // Create a MockClient using the Mock class provided by the Mockito package.
 // Create new instances of this class in each test.
 class MockClient extends Mock implements http.Client {}
 
 main() {
-  var client;
+  http.Client client;
+  MyRepository testRepository;
 
   setUp(() {
     client = MockClient();
+    testRepository = MyRepository(client);
   });
 
   group('fetchPost', () {
@@ -19,24 +22,37 @@ main() {
 
     test('returns a Post if the http call completes successfully', () async {
       //Arrange
-      when(client.get(requestString))
+      when(client.get(any))
           .thenAnswer((_) async => http.Response('{"title": "Test"}', 200));
       //Act
-      var response = await fetchPost(client, requestString: requestString);
+      var response =
+          await testRepository.fetchPost(requestString: requestString);
       //Assert
       //https://stackoverflow.com/questions/13298969/how-do-you-unittest-exceptions-in-dart
       expect(response, isA<Post>());
+      verify(client.get('https://jsonplaceholder.typicode.com/posts/1'));
+    });
+
+    test('returns data call completes successfully', () async {
+      //Arrange
+      when(client.get(any))
+          .thenAnswer((_) async => http.Response('{"title": "Test"}', 200));
+      //Act
+      var response =
+          await testRepository.fetchPost(requestString: requestString);
+      //Assert
       expect(response.data['title'], "Test");
     });
 
-    /*test('throws an exception if the http call completes with an error', () {
+    test('throws an exception if the http call completes with an error', () {
       //Arrange
       when(client.get(requestString))
           .thenAnswer((_) async => http.Response('Not found', 404));
       //Act
 
       //Assert
-      expect(fetchPost(client, requestString: requestString), throwsException);
-    });*/
+      expect(testRepository.fetchPost(requestString: requestString),
+          throwsA(const matcher.TypeMatcher<FetchPostRequestException>()));
+    });
   });
 }
